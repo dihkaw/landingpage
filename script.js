991bdbdb3bc9afb9231bc2aa1d3780e21c4ffd3c
dihkaw <162821190+dihkaw@users.noreply.github.com>
2025-03-04 08:24:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website Loaded!");
-
-    const skills = document.querySelectorAll('.skill');
-    skills.forEach(skill => {
-        skill.addEventListener('mouseenter', () => {
-            skill.style.transform = "scale(1.1)";
-            skill.style.transition = "0.3s";
-        });
-
-        skill.addEventListener('mouseleave', () => {
-            skill.style.transform = "scale(1)";
-        });
-    });
 });
